8f43814e00d62a6b175fd0b4158d70c51c840666
Vidit Jain <viditjain5598@gmail.com>
2019-10-15 14:55:27 +0530

    add Perilism dictionary and a CLI

---
+++ epigrams/__init__.py
@@ -0,0 +1,1 @@
+from .perilism import Perilisms
---
+++ main.py
@@ -0,0 +1,5 @@
+from epigrams import Perilisms
+import random
+
+ind = random.randrange(0, 120, 1)
+print("Epigram #{}\n".format(ind+1) + Perilisms[ind])
